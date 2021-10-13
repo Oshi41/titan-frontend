9a1cc47276ca6403b765aa13e2fa03fdf23585d5
oshi4 <sheglovaoe@gmail.com>
2021-10-13 16:46:26 +0300

    Fixed downloading button

--- src/view/MainView.tsx
+++ src/view/MainView.tsx
@@ -8,96 +8,81 @@ import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 import {TabRoutes} from '../App';
 import {StoreType} from '../types';
-import { BaseUrl } from '../api/1.0';
+import {BaseUrl} from '../api/1.0';
+import {getToken} from "../utils/index";
 
 interface Props {
-    setTab: (tab: TabRoutes) => void;
-    store: StoreType | undefined;
+  setTab: (tab: TabRoutes) => void;
+  store: StoreType | undefined;
 }
 
 export const MainView = (props: Props): JSX.Element => {
 
-    const [login, setLogin] = useControlledCookieState('titan_login', '');
+  const token = getToken();
 
-    const onClick = React.useCallback(() => {
-        const filename = 'Titan Launcher.jar';
-        const a = document.createElement("a");
-        a.href = BaseUrl + '/download?' + new URLSearchParams([['file', filename]]).toString();
-        a.setAttribute("download", filename);
-        a.click();
-        // api.get('/launcher', ['file', 'Titan Launcher.jar'])
-        //     .then(x => {
-        //         if (x.status == 200){
-        //             return x.blob();
-        //         }
-        //
-        //         throw new Error(x.status + '');
-        //     })
-        //     .then(x => {
-        //         const url = window.URL.createObjectURL(x);
-        //         const link = document.createElement('a');
-        //         link.href = url;
-        //         link.setAttribute('download', 'Titan Launcher.jar');
-        //         document.body.appendChild(link);
-        //         link.click();
-        //     });
-    }, []);
+  const onClick = React.useCallback(() => {
+    const filename = 'Titan Launcher.jar';
+    const a = document.createElement("a");
+    a.href = BaseUrl + '/download?' + new URLSearchParams([['file', filename]]).toString();
+    a.setAttribute("download", filename);
+    a.click();
+  }, []);
 
-    return (
-        <Grid container
-              direction="column"
-              justifyContent="space-between"
-              alignItems="flex-start"
-              rowSpacing={1}>
+  return (
+    <Grid container
+          direction="column"
+          justifyContent="space-between"
+          alignItems="flex-start"
+          rowSpacing={1}>
 
-            <Grid item>
-                <Typography sx={{fontSize: 24}} gutterBottom>
-                    Добро пожаловать на проект Titan!
-                </Typography>
-            </Grid>
+      <Grid item>
+        <Typography sx={{fontSize: 24}} gutterBottom>
+          Добро пожаловать на проект Titan!
+        </Typography>
+      </Grid>
 
-            {props.store === 'ely.by' && (
-                <Grid item>
-                    <Typography sx={{fontSize: 18}} gutterBottom>
-                        Проект использует авторизацию ely.by. Чтобы играть на проекте, необходимо использовать
-                        существующий аккаунт.
-                    </Typography>
-                </Grid>
-            )}
-
-            {!login && (
-                <Grid item>
-                    <Button variant="contained"
-                            onClick={() => props.setTab(TabRoutes.LOGIN)}
-                            sx={{fontSize: 18}}
-                            endIcon={<FingerprintIcon/>}>
-                        Войти
-                    </Button>
-                </Grid>
-            )}
-
-            {!login && (
-                <Grid item>
-                    <Button variant="contained"
-                            onClick={() => props.setTab(TabRoutes.REGISTRATION)}
-                            sx={{fontSize: 18}}
-                            endIcon={<PermIdentityIcon/>}>
-                        Зарегистрироваться
-                    </Button>
-                </Grid>
-            )}
-
-            {login && (
-                <Grid item>
-                    <Button variant="contained"
-                            onClick={onClick}
-                            sx={{fontSize: 18}}
-                            endIcon={<SportsEsportsIcon/>}>
-                        Скачать лаунчер
-                    </Button>
-                </Grid>
-            )}
-
+      {props.store === 'ely.by' && (
+        <Grid item>
+          <Typography sx={{fontSize: 18}} gutterBottom>
+            Проект использует авторизацию ely.by. Чтобы играть на проекте, необходимо использовать
+            существующий аккаунт.
+          </Typography>
         </Grid>
-    );
+      )}
+
+      {!token?.login && (
+        <Grid item>
+          <Button variant="contained"
+                  onClick={() => props.setTab(TabRoutes.LOGIN)}
+                  sx={{fontSize: 18}}
+                  endIcon={<FingerprintIcon/>}>
+            Войти
+          </Button>
+        </Grid>
+      )}
+
+      {!token?.login && (
+        <Grid item>
+          <Button variant="contained"
+                  onClick={() => props.setTab(TabRoutes.REGISTRATION)}
+                  sx={{fontSize: 18}}
+                  endIcon={<PermIdentityIcon/>}>
+            Зарегистрироваться
+          </Button>
+        </Grid>
+      )}
+
+      {token?.login && (
+        <Grid item>
+          <Button variant="contained"
+                  onClick={onClick}
+                  sx={{fontSize: 18}}
+                  endIcon={<SportsEsportsIcon/>}>
+            Скачать лаунчер
+          </Button>
+        </Grid>
+      )}
+
+    </Grid>
+  );
 }
